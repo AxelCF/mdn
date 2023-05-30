2aebc7d42fec336a504bae2a1888fd9cbdab806e
AxelCF <95211943+AxelCF@users.noreply.github.com>
2023-05-30 21:08:13 +0200

    tp story

---
+++ Generateur_d'histoires_absurdes/main.js
@@ -0,0 +1,65 @@
+// 1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
+
+const customName = document.getElementById("customname");
+const randomize = document.querySelector(".randomize");
+const story = document.querySelector(".story");
+
+function randomValueFromArray(array) {
+  const random = Math.floor(Math.random() * array.length);
+  return array[random];
+}
+
+// 2. RAW TEXT STRINGS
+
+// It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
+let storyText =
+  "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
+// Willy the Goblin
+// Big Daddy
+// Father Christmas
+let insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
+// the soup kitchen
+// Disneyland
+// the White House
+let insertY = ["the soup kitchen", "Disneyland", "the White House"];
+// spontaneously combusted
+// melted into a puddle on the sidewalk
+// turned into a slug and crawled away
+let insertZ = [
+  "spontaneously combusted",
+  "melted into a puddle on the sidewalk",
+  "turned into a slug and crawled away",
+];
+
+// 3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
+
+randomize.addEventListener("click", result);
+console.log();
+
+function result() {
+  let newStory = storyText;
+
+  if (customName.value !== "") {
+    const name = customName.value;
+    newStory = newStory.replace("Bob", name);
+  }
+
+  if (document.getElementById("uk").checked) {
+    const weight = Math.round(300 / 14) + " stone"; //diviser la valeur "masse" par 14
+    const temperature = Math.round(((94 - 32) * 5) / 9) + " centigrade"; //(32 °F − 32) × 5/9 = 0 °C
+    newStory = newStory.replace("94 fahrenheit", temperature);
+    newStory = newStory.replace("300 pounds", weight);
+  }
+
+  let xItem = randomValueFromArray(insertX);
+  let yItem = randomValueFromArray(insertY);
+  let zItem = randomValueFromArray(insertZ);
+
+  newStory = newStory.replace(":insertx:", xItem);
+  newStory = newStory.replace(":insertx:", xItem);
+  newStory = newStory.replace(":inserty:", yItem);
+  newStory = newStory.replace(":insertz:", zItem);
+
+  story.textContent = newStory;
+  story.style.visibility = "visible";
+}
